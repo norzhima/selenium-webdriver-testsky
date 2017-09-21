831c75f88cfb422ed4cedfef2511fe799104ac4c
Norjima Chagdurova <n.chagdurova@skyway.capital>
2017-09-21 15:24:47 +0300

    indication of the right xpath

--- lkswc_config.py
+++ lkswc_config.py
@@ -1,4 +1,4 @@
-main_url = 'https://cab-test7.skyway.capital/auth/login'
+main_url = 'https://cab-test7.skyway.capital'
 delay = 5
 default_browser = 'Chrome'
 
@@ -6,9 +6,9 @@ login = 'n.chagdurova@skyway.capital'
 password = 'Xfulehjdf!1'
 username = 'Norzhima Чагдурова'
 
-language_search_xpath = "//input[@name='LoginForm[email]']"
-switching_to_ru_xpath = "//input[@name='LoginForm[password]']"
-login_field_xpath = "(//BODY[@onload='preload_page()']//INPUT[@class='login__form-control'])[1]"
-passw_field_xpath = "(//BODY[@onload='preload_page()']//INPUT[@class='login__form-control'])[2]"
-login_button_xpath = "//BODY[@onload='preload_page()']//INPUT[@id='buttonLoginSubmit']"
+language_search_xpath = "//DIV[@class='login__header-ul']//SPAN[@href='#']"
+switching_to_ru_xpath = "switching_to_ru_xpath = //DIV[@class='login__header-ul']//A[@href='#'][text()='RU']"
+login_field_xpath = "//input[@name='LoginForm[email]']"
+passw_field_xpath = "//input[@name='LoginForm[password]']"
+login_button_xpath = "//input[@id='buttonLoginSubmit']"
 username_xpath = "//h3[@class='personal-card__name personal-card__name_verified']"
