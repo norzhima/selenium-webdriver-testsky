72269a86ce390462cd4ffa2abcc12b11e1eafb43
Norjima Chagdurova <n.chagdurova@skyway.capital>
2017-09-21 15:15:55 +0300

    update xpath: email, password

--- lkswc_config.py
+++ lkswc_config.py
@@ -6,8 +6,8 @@ login = 'n.chagdurova@skyway.capital'
 password = 'Xfulehjdf!1'
 username = 'Norzhima Чагдурова'
 
-language_search_xpath = "//DIV[@class='login__header-ul']//SPAN[@href='#']"
-switching_to_ru_xpath = "//DIV[@class='login__header-ul']//A[@href='#'][text()='RU']"
+language_search_xpath = "//input[@name='LoginForm[email]']"
+switching_to_ru_xpath = "//input[@name='LoginForm[password]']"
 login_field_xpath = "(//BODY[@onload='preload_page()']//INPUT[@class='login__form-control'])[1]"
 passw_field_xpath = "(//BODY[@onload='preload_page()']//INPUT[@class='login__form-control'])[2]"
 login_button_xpath = "//BODY[@onload='preload_page()']//INPUT[@id='buttonLoginSubmit']"
