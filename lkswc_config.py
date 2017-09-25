c835dc334080be2ba5c36bc97d98cf560587a97c
Norjima Chagdurova <n.chagdurova@skyway.capital>
2017-09-25 12:31:36 +0300

    added in .gitignore lkswc_private_data

--- lkswc_config.py
+++ lkswc_config.py
@@ -2,9 +2,7 @@ main_url = 'https://cab-test7.skyway.capital'
 delay = 5
 default_browser = 'Chrome'
 
-login = 'n.chagdurova@skyway.capital'
-password = 'Xfulehjdf!1'
-username = 'Norzhima Чагдурова'
+
 
 language_search_xpath = "//DIV[@class='login__header-ul']//SPAN[@href='#']"
 switching_to_ru_xpath = "switching_to_ru_xpath = //DIV[@class='login__header-ul']//A[@href='#'][text()='RU']"
